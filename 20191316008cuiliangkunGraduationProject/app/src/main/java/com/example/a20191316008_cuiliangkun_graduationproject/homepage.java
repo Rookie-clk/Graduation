0ceca1d2d7e399a9dd1659fc91043808cf006315
clk <401048977@qq.com>
2021-01-18 09:26:02 +0800

    迷之bug
    主页搜索栏出现两个按钮

--- 20191316008cuiliangkunGraduationProject/app/src/main/java/com/example/a20191316008_cuiliangkun_graduationproject/homepage.java
+++ 20191316008cuiliangkunGraduationProject/app/src/main/java/com/example/a20191316008_cuiliangkun_graduationproject/homepage.java
@@ -43,7 +43,7 @@ public class homepage extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.homepage,container,false);
+        view = inflater.inflate(R.layout.activity_homepage,container,false);
 
         if (view!=null){
             initView();
