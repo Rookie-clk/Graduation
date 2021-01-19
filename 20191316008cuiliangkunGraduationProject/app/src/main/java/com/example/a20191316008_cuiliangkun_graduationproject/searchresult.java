cc346c8d720aa073ea7f6c259c6326eccb2d966a
clk <401048977@qq.com>
2021-01-19 15:45:47 +0800

    搜索详情页listview

--- 20191316008cuiliangkunGraduationProject/app/src/main/java/com/example/a20191316008_cuiliangkun_graduationproject/searchresult.java
+++ 20191316008cuiliangkunGraduationProject/app/src/main/java/com/example/a20191316008_cuiliangkun_graduationproject/searchresult.java
@@ -2,6 +2,7 @@ package com.example.a20191316008_cuiliangkun_graduationproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,11 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class searchresult extends AppCompatActivity {
+public class searchresult extends AppCompatActivity implements View.OnClickListener {
 
     private int[] icons = {R.drawable.beach,R.drawable.cyber,R.drawable.pool};
     private String[] details = {"原木双1.8米大床浴缸房",
@@ -23,47 +25,78 @@ public class searchresult extends AppCompatActivity {
     private String[] prices = {"￥628","￥455","￥575"};
 
     private ListView listView;
+    private Button detailbtn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchresult);
-//        listView = findViewById(R.id.searchresult_listview);
-//        listView.setAdapter(new SearchBaseAdapter());
+
+        listView = findViewById(R.id.searchresult_listview);
+        listView.setAdapter(new SearchBaseAdapter());
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+//        switch (v.getId()){
+//            case R.id.searchresult_btn:
+//                int hotelid = 0;
+//                Intent intent = new Intent(searchresult.this,hotelpage.class);
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("hotelid",hotelid);
+//                intent.putExtras(bundle);
+//                startActivity(intent);
+//                Toast.makeText(searchresult.this,"123",Toast.LENGTH_SHORT).show();
+//                break;
+//        }
     }
 
 
-//    private class SearchBaseAdapter extends BaseAdapter {
-//
-//        @Override
-//        public int getCount() {
-//            return icons.length;
-//        }
-//
-//        @Override
-//        public Object getItem(int position) {
-//            return icons[position];
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return position;
-//        }
-//
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            View  view = View.inflate(searchresult.this,R.layout.hotel_item,null);
-//
-//            ImageView icon = findViewById(R.id.searchresult_icon);
-//           TextView hotelname = findViewById(R.id.searchresult_name);
-//            TextView price = findViewById(R.id.searchresult_price);
-//
-//            hotelname.setText(details[position]);
-//            price.setText(prices[position]);
-//            icon.setImageResource(icons[position]);
-//
-//            return view;
-//        }
-//    }
+    private class SearchBaseAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return icons.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return icons[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View  view = View.inflate(searchresult.this,R.layout.hotel_item,null);
+            ImageView icon = view.findViewById(R.id.searchresult_icon);
+           TextView hotelname = view.findViewById(R.id.searchresult_name);
+            TextView price = view.findViewById(R.id.searchresult_price);
+            Button detailbtn = view.findViewById(R.id.searchresult_btn);
+            detailbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int hotelid = 0;
+                Intent intent = new Intent(searchresult.this,hotelpage.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("hotelid",hotelid);               //搜索结果页跳转至详情页
+                intent.putExtras(bundle);
+                startActivity(intent);
+                }
+            });
+            hotelname.setText(details[position]);
+            price.setText(prices[position]);
+            icon.setImageResource(icons[position]);
+
+            return view;
+        }
+
+
+    }
 }
